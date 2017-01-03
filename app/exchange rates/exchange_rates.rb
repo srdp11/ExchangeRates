ca89972c24199bbe1ae356fa37e2a7d8149e643a
Michael Orlov <overdzen@gmail.com>
2017-01-03 16:01:23 +0300

    Add gem pry and join methods for extracting sell/buy info and calculating spread to single methods

--- app/exchange rates/exchange_rates.rb
+++ app/exchange rates/exchange_rates.rb
@@ -3,41 +3,24 @@ require 'json'
 
 class ExchangeRates
   @@uri = URI('https://www.tinkoff.ru/api/v1/currency_rates')
-  @json_data
-  @rates_data_usd
-  @rates_data_eur
 
-  def request_data
+  def fetch_data(currency)
     response = Net::HTTP.get(@@uri)
-    @json_data = JSON.parse(response)
+    json_data = JSON.parse(response)
 
-    @rates_data_eur = @json_data['payload']['rates'].find { |child| child['category'] == 'DepositPayments' &&
-                                                                    child['fromCurrency']['name'] == 'EUR' &&
-                                                                    child['toCurrency']['name'] == 'RUB'
-                                                          }
-
-    @rates_data_usd = @json_data['payload']['rates'].find { |child| child['category'] == 'DepositPayments' &&
-                                                                    child['fromCurrency']['name'] == 'USD' &&
-                                                                    child['toCurrency']['name'] == 'RUB'
-                                                          }
+    rates_data = json_data['payload']['rates'].find { |child| child['category'] == 'DepositPayments' &&
+                                                              child['fromCurrency']['name'] == currency &&
+                                                              child['toCurrency']['name'] == 'RUB'
+                                                    }
   end
 
-  def get_usd_sell_buy
-    [@rates_data_usd['sell'], @rates_data_usd['buy']]
+  def get_sell_buy(rates_data)
+    [rates_data['sell'], rates_data['buy']]
   end
 
-  def get_eur_sell_buy
-    [@rates_data_eur['sell'], @rates_data_eur['buy']]
-  end
-
-  def get_usd_spread
-    [@rates_data_usd['sell'] - @rates_data_usd['buy'],
-     (@rates_data_usd['sell'] - @rates_data_usd['buy']) / @rates_data_usd['buy']]
-  end
-
-  def get_eur_spread
-    [@rates_data_eur['sell'] - @rates_data_eur['buy'],
-     (@rates_data_eur['sell'] - @rates_data_eur['buy']) / @rates_data_eur['buy']]
+  def get_spread(rates_data)
+    [rates_data['sell'] - rates_data['buy'],
+     (rates_data['sell'] - rates_data['buy']) / rates_data['buy']]
   end
 end
 
