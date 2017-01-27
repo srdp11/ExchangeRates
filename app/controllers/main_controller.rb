7bdac8814c5211ae283d4d7c1e98f6e255cd6acc
Michael Orlov <overdzen@gmail.com>
2017-01-27 22:06:31 +0300

    Add percision (2 digits) for rates

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,21 +2,19 @@ require 'Rate'
 require 'pry'
 
 class MainController < ApplicationController
-  @@from_to_vals = ['EUR/RUB', 'EUR/USD', 'EUR/GBR',
-                    'USD/RUB', 'USD/EUR', 'USD/GBR',
-                    'GBR/RUB', 'GBR/USD', 'GBR/EUR']
+  @@from_to_vals = ['EUR/RUB', 'EUR/USD', 'EUR/GBP',
+                    'USD/RUB', 'USD/EUR', 'USD/GBP']
 
   def index
-    # TODO change 'eur' => 'RUB' to default page selecting for rates
     from, to = @@from_to_vals[0].scan(/\w+/)
 
     rates = ExchangeRates.new
     data = rates.from_to(rates.fetch_data, from, to)
 
-    sell_buy_data = rates.sell_buy(data)
+    sell_buy_data = rates.sell_buy(data).inject({}) { |h, (k, v)| h[k] = '%.2f' % v; h }
     spread_data = rates.spread(data).inject({}) { |h, (k, v)| h[k] = '%.2f' % v; h }
-    puts spread_data
 
+    # TODO add information about forecast and average rates
     @sell = "#{sell_buy_data[:sell]} #{to}"
     @buy = "#{sell_buy_data[:buy]} #{to}"
     @spread = "#{spread_data[:absolute]} #{to} (#{spread_data[:relative]} %)"
