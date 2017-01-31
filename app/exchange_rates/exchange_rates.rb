eca56f8edfb97a64da1c580966e140269b31ab0b
Michael Orlov <overdzen@gmail.com>
2017-01-31 12:04:04 +0300

    Add method to extracting data about all rates in formatting strings

--- app/exchange_rates/exchange_rates.rb
+++ app/exchange_rates/exchange_rates.rb
@@ -27,4 +27,24 @@ class ExchangeRates
   def spread(data)
     { absolute: data['sell'] - data['buy'], relative: (data['sell'] - data['buy']) / data['buy'] }
   end
+
+  def fetch_format_data
+    rates_data = Hash.new
+
+    ExchangeRates::AVALIBLE_RATES.each do |rate|
+      from, to = rate.scan(/\w+/)
+
+      rates = ExchangeRates.new
+      data = rates.from_to(rates.fetch_data, from, to)
+
+      sell_buy_data = rates.sell_buy(data).inject({}) { |h, (k, v)| h[k] = '%.2f' % v; h }
+      spread_data = rates.spread(data).inject({}) { |h, (k, v)| h[k] = '%.2f' % v; h }
+
+      rates_data[rate]  = { sell: "#{sell_buy_data[:sell]} #{to}",
+                            buy: "#{sell_buy_data[:buy]} #{to}",
+                            spread: "#{spread_data[:absolute]} #{to} (#{spread_data[:relative]} %)" }
+    end
+
+    rates_data
+  end
 end
